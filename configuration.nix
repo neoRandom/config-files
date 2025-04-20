a7adb917a8e83939a8bd56ea15eb3e06a0bcb07a
Fellipe Leonardo <112525075+neoRandom@users.noreply.github.com>
2025-04-20 18:32:38 -0300

    feat: enabled firewall

--- configuration.nix
+++ configuration.nix
@@ -148,6 +148,11 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ ];
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
